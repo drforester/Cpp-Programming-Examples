2deec37f476e6ee121000a36d7d11a4c723f9e03
drforester <d.raymond.forester@gmail.com>
2020-03-13 13:20:58 -0500

    misc_utils

---
+++ src/utils/misc_utils.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+std::vector<std::string> split_string(const std::string& str,
+                                      const std::string& delimiter) {
+    std::vector<std::string> strings;
+
+    std::string::size_type pos = 0;
+    std::string::size_type prev = 0;
+    while ((pos = str.find(delimiter, prev)) != std::string::npos) {
+        strings.push_back(str.substr(prev, pos - prev));
+        prev = pos + 1;
+    }
+
+    // To get the last substring (or only, if delimiter is not found)
+    strings.push_back(str.substr(prev));
+
+    return strings;
+}
---
+++ src/utils/misc_utils.hpp
@@ -0,0 +1,12 @@
+#ifndef MISC_UTILS_HPP
+#define MISC_UTILS_HPP
+
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+vector<string> split_string(const string& str, const string& delimiter);
+
+#endif
