289d20f47ce698381ba0118d41055813a4cc0fc4
drforester <d.raymond.forester@gmail.com>
2020-03-13 13:25:05 -0500

    misc_utils

--- src/utils/misc_utils.cpp
+++ src/utils/misc_utils.cpp
@@ -19,3 +19,18 @@ std::vector<std::string> split_string(const std::string& str,
 
     return strings;
 }
+
+
+/*
+example of usage:
+
+    #include misc_utils.hpp
+
+    ...
+    vector<string> fnVec1 = split_string(filename, "/"); // split on dir separator
+    string fnStr1 = fnVec1[fnVec1.size()-1]; // last elem in vector
+    vector<string> fnVec2 = split_string(fnStr1, "."); // split on file extension dot
+    string fnStr2 = fnVec2[0]; // first elem in vector
+    cout << "FILENAME THING : " << fnStr2 << "\n";
+
+*/
