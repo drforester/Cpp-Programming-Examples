f2a100cd66034dc268600768cc4832f6dc8c3a3d
drforester <d.raymond.forester@gmail.com>
2020-03-25 09:47:59 -0500

    longest palindromic ...

--- src/palindromic.cpp
+++ src/palindromic.cpp
@@ -24,17 +24,31 @@ int main()
     std::getline(std::cin, inStr); // for testing only XXXXXXXXX
 
     int n = inStr.size();
-    string pal = "";
+    string pal, longest_pal;
     int dist, i1, i2;
+
+    // center is a char
     for (int i=1; i<n-1; i++) {
         dist = std::min(i, n-1-i);
-        cout << dist << " ";
+        cout << "i:" << i << " dist:" << dist << " ";
         for (int j=1; j<dist+1; j++) {
-            cout << "   " << j;
+            cout << "j:" << j << " ";
+            if (inStr[i-j] != inStr[i+j]) {
+                break;
+            }
+            else {
+                pal = inStr.substr(i-j, 2*j+1);
+            }
         }
+        if (pal.size() > longest_pal.size())
+            longest_pal = pal;
         cout << "\n";
     }
 
+    // center is between two chars
+    // ...
+
+    cout << "longest pal: " << longest_pal << "\n";
 }
 
 
