595a098380d7a60090a2665c31bfb4bb719efe7d
drforester <d.raymond.forester@gmail.com>
2020-03-25 20:31:33 -0500

    greatest palindromic substring

--- src/palindromic.cpp
+++ src/palindromic.cpp
@@ -20,35 +20,40 @@ using std::string;
 
 int main()
 {
-    string inStr;
-    std::getline(std::cin, inStr); // for testing only XXXXXXXXX
+    string s;
+    std::getline(std::cin, s); // for testing only XXXXXXXXX
 
-    int n = inStr.size();
-    string pal, longest_pal;
-    int dist, i1, i2;
+    int n = s.size();
+    int greatest_len = 0;
+    string longest_pal = "";
 
-    // center is a char
-    for (int i=1; i<n-1; i++) {
-        dist = std::min(i, n-1-i);
-        cout << "i:" << i << " dist:" << dist << " ";
-        for (int j=1; j<dist+1; j++) {
-            cout << "j:" << j << " ";
-            if (inStr[i-j] != inStr[i+j]) {
+    // case 1: middle "mid" is a char
+    for(int mid = 0; mid < n; mid++) {
+        for(int x = 0; mid - x >= 0 && mid + x < n; x++) {
+            if(s[mid-x] != s[mid+x]) {
                 break;
             }
-            else {
-                pal = inStr.substr(i-j, 2*j+1);
+            int len = 2 * x + 1;
+            if(len > greatest_len) {
+                greatest_len = len;
+                longest_pal = s.substr(mid - x, len);
             }
         }
-        if (pal.size() > longest_pal.size())
-            longest_pal = pal;
-        cout << "\n";
     }
 
-    // center is between two chars
-    // ...
+    // case 2: middle "mid" is between two chars
+    for(int mid = 0; mid < n - 1; mid++) {
+        for(int x = 1; mid - x + 1 >= 0 && mid + x < n; x++) {
+            if(s[mid-x+1] != s[mid+x]) {
+                break;
+            }
+            int len = 2 * x;
+            if(len > greatest_len) {
+                greatest_len = len;
+                longest_pal = s.substr(mid - x + 1, len);
+            }
+        }
+    }
 
-    cout << "longest pal: " << longest_pal << "\n";
+    cout << longest_pal << "\n";
 }
-
-
